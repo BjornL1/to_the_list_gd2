86672c6797fc694620da18a55c550737727e43ad
Björn Larsson <robjorn_larsson@hotmail.com>
2024-02-19 15:10:00 +0000

    Update views and urls to verify file connections

--- user_authentication/views.py
+++ user_authentication/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    message = "Welcome to our website!"  # Define the message to display
+    return HttpResponse(message)
