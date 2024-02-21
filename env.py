b00b080af4d124295f2d45a6b7253d4733231a62
Björn Larsson <robjorn_larsson@hotmail.com>
2024-02-21 06:23:55 +0000

    Update secret key settings

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgres://vlyvztyn:X9HkBTWq7b0yHdYSGfbguLaT1LjKZPBu@snuffleupagus.db.elephantsql.com/vlyvztyn")
+os.environ.setdefault("SECRET_KEY", "o8wdix(jgpwpuvsy4!v-ds98whtb0eqbo7n84g%(-l!9_zv@xk")
--- to_the_list/settings.py
+++ to_the_list/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=$y3wjj_dqr4h)--yhgf)9sy&xevf46stji2&xf3qtqpoggf_v'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
