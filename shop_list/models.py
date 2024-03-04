d471cb11dc3026f90537ccd253f7a34f692cbaa4
Björn Larsson <robjorn_larsson@hotmail.com>
2024-03-04 14:15:47 +0000

    Add is done to models to enable implementation of logic for items later on

--- shop_list/models.py
+++ shop_list/models.py
@@ -20,6 +20,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     quantity = models.IntegerField(default=1)
     shopping_list = models.ForeignKey(ShoppingList, on_delete=models.CASCADE)
+    is_done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
